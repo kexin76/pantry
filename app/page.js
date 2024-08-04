fd70349e6874bae01d0462e95f66f4d81fd829f2
kexin76 <kexincx@gmail.com>
2024-08-03 23:53:59 -0400

    updated edit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 'use client';
 import React, {useState, useEffect} from "react";
-import { collection, addDoc, getDoc, QuerySnapshot, query, onSnapshot, deleteDoc, doc,} from "firebase/firestore"
+import { collection, addDoc, getDoc, QuerySnapshot, query, onSnapshot, deleteDoc, doc, setDoc, updateDoc} from "firebase/firestore"
 import {db} from './firebase'
 import { Box, Container, FormControl, InputLabel, Typography, Input, FormGroup, Button, OutlinedInput, Grid, List, ListItem, ListItemButton, Divider, ToggleButton, TextField,} from "@mui/material";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { blue } from "@mui/material/colors";
 
 export default function Home() {
   const [items, setItems] = useState([
@@ -51,12 +50,27 @@ export default function Home() {
     await deleteDoc(doc(db, "items", id));
   };
 
-  const updateItem = async (id) => {
-
-  };
 
   const [searchInput, setSearchInput] = useState("")
   const filteredItems = items.filter(item => item.name.toLowerCase().startsWith(searchInput.toLowerCase()));
+
+  const [selectEdit, setSelectEdit] = useState(false)
+  function clickEdit() {
+    setSelectEdit(!selectEdit);
+    console.log(selectEdit);
+  };
+
+  // Needs fixing
+  const updateQuant = async (id, e) => {
+    if(e != ""){
+      await updateDoc(doc(db, "items", id), {
+        quant: e
+      });
+    }
+
+  }
+  
+
 
 
   // Theme / Color Maker
@@ -76,6 +90,8 @@ export default function Home() {
           <Box maxWidth={"64 rem"} fontSize={14} sx={{alignItems: "center", fontFamily: "monospace", justifyContent: "space-between", width:"100%"}}>
             <Typography align="center" variant="h2" sx={{fontFamily: "Segoe UI Emoji",}}>Pantry</Typography>
             <br/><br/>
+
+            {/* Enter Item and Quanity. And Submit button */}
             <Box padding={2} bgcolor={"#708090"} sx={{borderRadius: 2,}}>
               <Box display="flex">
                 <FormControl sx={{color: "black", flexGrow: 1, margin: 1}}>
@@ -98,19 +114,20 @@ export default function Home() {
                   <Typography align="center" variant="h5" sx={{color:"#FFFFFF"}}>+</Typography> 
                 </Button>
               </Box>
-
-              {/* Edit button and search bar */}
-
+              
+              {/* Search and Edit */}
               <Grid>
                 <Grid py={1} px={2} >
                   <TextField value={searchInput || ""} onChange={(e) => {setSearchInput(e.target.value)}} label="Search" variant="filled" size="small" color="slate" sx={{bgcolor:"#a1a1aa", borderColor:"#a1a1aa", borderRadius:1,}} />
                   
-                  <ToggleButton sx={{bgcolor:"#166534", paddingX:3, py:1, float:"right"}}>
+                  {/* Make edit button functional */}
+                  <ToggleButton onClick={clickEdit} sx={{bgcolor:"#166534", paddingX:3, py:1, float:"right"}}>
                     <Typography variant="h6">Edit</Typography> 
                   </ToggleButton>
                 </Grid>
               </Grid>
-
+              
+              {/* Header for list */}
               <Box paddingTop={1} px={2} sx={{justifyContent:"space-between", display:"flex"}}>
                 <Grid py={3/2} paddingLeft={2} paddingRight={1} bgcolor={"rgb(2 6 23)"} container spacing={0}>
                   <Grid xs={10}>
@@ -126,7 +143,8 @@ export default function Home() {
               </Box>
               
               <Divider sx={{marginX:2, bgcolor:"#cbd5e1" }} />
-
+              
+              {/* List all items */}
               <List sx={{paddingY:0}}>
                 {filteredItems.map((item, id) => (
                   <>
@@ -135,8 +153,18 @@ export default function Home() {
                       <Grid>
                         <Grid textTransform={"capitalize"}>{item.name}</Grid>
                       </Grid>
+
                       <Grid>
-                        <Grid>{item.quant}</Grid>
+                        {selectEdit == false ? <Grid>{item.quant}</Grid> : 
+                        // <TextField label="Search" variant="standard" size="small" color="slate" sx={{bgcolor:"#a1a1aa", borderColor:"#a1a1aa",}} />
+                        <>
+                          <label for="editQuant"></label>
+                          {/* Needs fixing I think */}
+                          <input type="text" id="editQuant" onChange={(e) => updateQuant(item.id, e)} placeholder={item.quant} className="bg-slate-700 rounded-md" />
+                        </>
+                        
+                        }
+                        
                       </Grid>
                     </Box>
                     <ListItemButton disableElevation sx={{bgcolor:"rgb(2 6 23)", borderLeft:2,  borderColor:"#0f172a", "&:not(:hover)":{bgcolor:"rgb(2 6 23)"} , "&:hover": {bgcolor:"#0f172a",}}} onClick={() => deleteItem(item.id)}>
@@ -149,11 +177,13 @@ export default function Home() {
                   </>
                 ))}
               </List>
+
+              {/* Total amount of items */}
               {items.length < 1 ? ('') : (
                 <div className="flex justify-between p-3">
                   <span>Total Amount of Items</span>
                   <span>{total}</span>
-                  <span>{searchInput}</span>
+                  <span>{selectEdit}</span>
                 </div>
               )}
             </Box>
